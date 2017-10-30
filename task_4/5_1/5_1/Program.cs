c46502b0f0d6036c93b55c296857397aad932916
moyui <gxy1510@gmail.com>
2017-10-30 21:25:46 +0800

    2017/10/30

---
+++ task_4/5_1/5_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5_1
+{
+    class Program
+    {
+        static Boolean Judge(int value, int[] arr) 
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int value;
+            for (int i = 0; i < 10; i++)
+            {
+                do
+                {
+                    Random rnd = new Random();
+                    value = rnd.Next(50, 99);
+                } while (Judge(value, arr));
+                arr[i] = value;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}

c6619c07ea471a49b97cb5231033b8263c4cea1b
moyui <gxy1510@gmail.com>
2017-12-07 22:31:09 +0800

    2017/12/7

---
+++ task_8/9_5/9_5/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _9_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "图像文件|*.bmp";
+            DialogResult myresult = openFileDialog1.ShowDialog();
+            if (myresult == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.ImageLocation = openFileDialog1.FileName;
+                    FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                    BinaryReader br = new BinaryReader(fs);
+                    byte[] bmpfile = br.ReadBytes((int)fs.Length);
+                    fs.Close();
+                    for (int i = 0; i < bmpfile.Length; i++)
+                    {
+                        richTextBox1.Text += bmpfile[i].ToString("X") + " ";
+                    }
+                }
+                catch 
+                {
+                    richTextBox1.Text = "文件打开错误";
+                }
+            }
+        }
+    }
+}
---
+++ task_8/9_6/9_6/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _9_6
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string path = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == folderBrowserDialog1.ShowDialog())
+            {
+                path = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = path;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo source = new DirectoryInfo(path);
+            DirectoryInfo destination = new DirectoryInfo(textBox2.Text);
+            if (!destination.Exists)
+            {
+                destination.Create();
+            }
+            foreach (FileInfo f in source.GetFiles())
+            {
+                string[] arr = f.FullName.Split('.');
+                if (arr[arr.Length  - 1])
+            }
+        }
+    }
+}

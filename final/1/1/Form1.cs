6b9d8d479ea35ab26ee5ba6f4751db21e7c872f2
moyui <gxy1510@gmail.com>
2017-12-28 22:57:57 +0800

    2017/12/28

---
+++ final/1/1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text += "高夕毓";
+            label2.Text += "1513490428";
+        }
+    }
+}
---
+++ final/2/2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int num = 0;
+        int count = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            num = rd.Next(1, 101);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int answer = int.Parse(textBox1.Text);
+            count++;
+            if (count < 20)
+            {
+                if (answer < num) 
+                {
+                    label2.Text = "猜小了";
+                } else if(answer > num){
+                    label2.Text = "猜大了";
+                } else {
+                    label2.Text = "恭喜你答对了！猜的次数为" + count;
+                }
+            } else 
+            {
+                textBox1.Enabled = false;
+                label2.Text = "超过20次啦！";
+            }
+        }
+    }
+}
